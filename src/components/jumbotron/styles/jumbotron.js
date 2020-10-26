c4855fb260fa3ed630b9a63ddec8e57dbd2e0083
Fen <fen@Pinsons-MacBook-Pro.local>
2020-10-26 15:22:44 +0100

    accordion

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -14,9 +14,12 @@ export const Inner = styled.div`
   flex-direction: ${({ direction }) => direction};
   justify-content: space-between;
   max-width: 1100px;
+  margin: auto;
+  width: 100%;
 
   @media (max-width: 1000px) {
     flex-direction: column;
+    margin: 10px;
   }
 `;
 
